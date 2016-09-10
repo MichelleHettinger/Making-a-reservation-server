622f59a5887b0b6f2ca6703a1e7ff98cc71d427a
Michelle Hettinger <Michelle Hettinger>
2016-09-10 13:44:31 -0400

    current and waiting lists now display on webpage

--- app/data/waitinglist-data.js
+++ app/data/waitinglist-data.js
@@ -1,3 +1,8 @@
-var waitingArray = [];
+var waitingArray = [{
+	customerName: "Michelle Hettinger",
+	phoneNumber: "321-422-9522",
+	customerEmail:"michelleahettinger@gmail.com",
+	customerID: "7779996655"
+}];
 
 module.exports = waitingArray;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,16 +3,29 @@ var waitings = require("../data/waitinglist-data.js");
 
 
 module.exports = function (app){
-	app.post('/api/tables', function(req, res){
-		// console.log(req.body);
 
-		tables.push(req.body);
-		console.log(tables);
-
+	app.get('/api/tables', function(req, res){
 		res.json(tables);
 	});
-
 	app.get('/api/waitlist', function(req, res){
+		res.json(waitings);
+		console.log(waitings);
+	});
+
+
+	app.post('/api/tables', function(req, res){
+
+		if (tables.length < 5){
+			tables.push(req.body);
+			res.json(true);
+		}
+		else {
+			waitings.push(req.body);
+			res.json(false);
+		}
+	});
+
+	app.post('/api/clear', function(req, res){
 		// res.sendFile(path.join(__dirname + '/app/public/reserve.html'));
 	});
 };
