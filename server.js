abd1967192d5c4bcd1050a4cd01f799c88975ee9
David Reel <davidpreel@gmail.com>
2016-09-10 11:41:39 -0400

    routes

--- server.js
+++ server.js
@@ -22,9 +22,13 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/app/public/home.html'));
 })
 
+app.get('/tables', function(req, res){
+	res.sendFile(path.join(__dirname + '/app/public/tables.html'));
+})
 
-
-
+app.get('/reserve', function(req, res){
+	res.sendFile(path.join(__dirname + '/app/public/reserve.html'));
+})
 
 // Starts the server to begin listening
 // =============================================================
